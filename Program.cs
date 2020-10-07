e39e38f7a2e23e311b61ac90dee4d4aeee8f85f2
Lazaro Menezes <lazaro.menezes@actminds.com>
2020-10-07 17:11:17 -0300

    Add Azure App Configuration setup

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace appconfig_examples
 {
@@ -20,6 +17,26 @@ namespace appconfig_examples
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration(config =>
+                    {
+                        var settings = config.Build();
+
+                        var connection = settings.GetConnectionString("AzureAppConfiguration");
+
+                        config.AddAzureAppConfiguration(options => {
+                            options.Connect(connection);
+
+                            options.ConfigureRefresh(refresh =>
+                            {
+                                refresh.Register("daysToShow", refreshAll: true);
+                                refresh.SetCacheExpiration(TimeSpan.FromSeconds(10));
+                            });
+
+                            options.Select(KeyFilter.Any, LabelFilter.Null);
+                            options.Select(KeyFilter.Any, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+                        });
+                    });
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
