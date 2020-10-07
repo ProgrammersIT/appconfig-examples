74e35e873c8eb2a9bf4f3666b345e4bec94385e7
Lazaro Menezes <lazaro.menezes@actminds.com>
2020-10-07 13:42:25 -0300

    Refactor to include app settings

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace appconfig_examples.Controllers
@@ -17,23 +17,30 @@ namespace appconfig_examples.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly int _daysToShow;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _daysToShow = configuration.GetValue<int>("daysToShow");
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogDebug("Starting to fetch data");
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            var data = Enumerable.Range(1, _daysToShow).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+            _logger.LogDebug("Data fetching done");
+            _logger.LogInformation("Returning data");
+            return data;
         }
     }
 }
